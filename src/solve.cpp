8e63333eb38a935ecc13b230fe9533b61bc1ae93
Dmitry Demin <dmitryy.demin@gmail.com>
2025-10-18 18:38:28 +0300

    6. Написать минимальную реализацию функции solve, которая удовлетворяет тесту из п.5.

--- src/solve.cpp
+++ src/solve.cpp
@@ -1,9 +1,10 @@
 #include "solve.h"
+#include <cmath>
 
 std::vector<double> solve(double a, double b, double c, double epsilon) {
     double D=b*b-4.*a*c;
     if(D<-epsilon) {
         return std::vector<double>();
     }
-    return std::vector<double>();
+    return std::vector<double>({ (-b-std::sqrt(D))/(2.*a), (-b+std::sqrt(D))/(2.*a) });
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,11 +4,11 @@
 const double epsilon=1e-7;
 
 TEST(solve, x_sq_plus_1) {
-    EXPECT_EQ(solve(1, 0, 1,epsilon), std::vector<double>());
+    EXPECT_EQ(solve(1, 0, 1, epsilon), std::vector<double>());
 }
 
 TEST(solve, x_sq_minus_1) {
-    EXPECT_EQ(solve(1, 0, -1,epsilon), std::vector<double>({1,-1}));
+    EXPECT_EQ(solve(1, 0, -1, epsilon), std::vector<double>({-1,1}));
 }
 
 
