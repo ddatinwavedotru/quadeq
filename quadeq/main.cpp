f727cc0379d4560d2f62993975cec53ce85a3177
Dmitry Demin <dmitryy.demin@gmail.com>
2025-10-18 16:19:48 +0300

    Add CMakeLists and main.cpp

---
+++ quadeq/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+std::vector<double> solve(double a, double v, double c) {
+    return std::vector<double>();
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
