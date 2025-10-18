9736d3a9116a0318e5a408fc7ccc02b063e6bd49
Dmitry Demin <dmitryy.demin@gmail.com>
2025-10-18 18:32:29 +0300

    5. Написать тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,6 +7,10 @@ TEST(solve, x_sq_plus_1) {
     EXPECT_EQ(solve(1, 0, 1,epsilon), std::vector<double>());
 }
 
+TEST(solve, x_sq_minus_1) {
+    EXPECT_EQ(solve(1, 0, -1,epsilon), std::vector<double>({1,-1}));
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
